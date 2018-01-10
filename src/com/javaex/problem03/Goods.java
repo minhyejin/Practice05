4c690d381bb873e8ae72e4039f431d38b9dd699f
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-10 14:41:06 +0900

    3

--- src/com/javaex/problem03/Goods.java
+++ src/com/javaex/problem03/Goods.java
@@ -7,5 +7,45 @@ public class Goods {
     private int count;
     
     
-    
+    public Goods() {}
+	public Goods(String name, int price, int count) {
+		this.name = name;
+		this.price = price;
+		this.count = count;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+
+	public int getCount() {
+		return count;
+	}
+
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+
+	public void showInfo() {
+    	System.out.println(name+" 가격:("+price+")이("+count+")개 입고되었습니다.");
+    }
 }
--- src/com/javaex/problem03/GoodsApp.java
+++ src/com/javaex/problem03/GoodsApp.java
@@ -1,14 +1,41 @@
 package com.javaex.problem03;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.List;
 import java.util.Scanner;
 
 public class GoodsApp {
 
     public static void main(String[] args) {
-
+    	Scanner scan = new Scanner(System.in);
+    	String name = "";
+    	int num = 3;
+       int price=0; 
+       int total=0;
+       int count=0;
        
+       List<Goods> goodsList = new ArrayList();
+       System.out.println("상품이름과"+"수량"+"을 입력해 주세요");
+       
+       for(int i =0; i <3; i++) {
+    	  name = scan.next();
+    	  price = scan.nextInt();
+    	  count = scan.nextInt();
+    	  Goods goods;
+    	  goods=new Goods(name,price,count);
+    	  goodsList.add(goods);
+    	   goods.showInfo();
+    	   
+       }
+       
+       for(int i=0; i<3; i++) {
+    	   Goods goods;
+   
+    	   goods=goodsList.get(i);
+    	   total += goods.getCount();
+    	   
+       }
+       System.out.println("모든 상품의 개수는 "+total+"입니다.");
     }
 
 }
