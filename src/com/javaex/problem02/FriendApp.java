46aaf923675a8c5e3ed4b83268d8b5a05f8b4d85
BIT-USER <BIT-USER@DESKTOP-VEEGU4M>
2018-01-10 13:40:53 +0900

    2

--- src/com/javaex/problem02/FriendApp.java
+++ src/com/javaex/problem02/FriendApp.java
@@ -1,9 +1,30 @@
 package com.javaex.problem02;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FriendApp {
 
     public static void main(String[] args) {
-
+    	Scanner scan = new Scanner(System.in);
+    	
+    	ArrayList<Friend> arr = new ArrayList<Friend>();
+    	for(int i =0; i<3; i++) {
+    		String info = scan.nextLine();
+    		
+    		String [] friend = info.split(" ");
+    		Friend fr = new Friend();
+    		fr.setName(friend[0]);
+    		fr.setHp(friend[1]);
+    		fr.setSchool(friend[2]);
+    		
+    		arr.add(fr);
+    		
+    		
+    	}for(Friend fr : arr) {
+    		fr.showInfo();
+    	}
+    	
        
     }
 
